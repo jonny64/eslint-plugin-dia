5f07ea0b172939f3c6045d7ec4065732aa59996b
Maksim Gruzdev <mgruzdev@cheby.ru>
2022-10-28 13:50:50 +0300

    no-mixed-column-width introduced

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     rules: {
         'no-ref-plural': require('./src/no-ref-plural'),
         'no-filter-is-deleted': require('./src/no-filter-is-deleted'),
+        'no-mixed-column-width': require('./src/no-mixed-column-width'),
     }
 }
